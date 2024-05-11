52b2ace46609520959cdec0e578dba8703571e60
Azam Mustufa Didagur <azammustafa66@gmail.com>
2024-05-11 15:45:21 +0530

    initial commit

---
+++ projects/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+import uuid
+
+
+class Project(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    featured_image = models.ImageField(null=True, blank=True, default="images/default.jpg")
+    votes = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
+    tags = models.ManyToManyField("Tag", blank=True)
+    demo_link = models.CharField(max_length=2000, null=True, blank=True)
+    source_link = models.CharField(max_length=2000, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """
+        This method returns the title of the project
+        """
+        return self.title
+
+
+class Review(models.Model):
+    VOTE_TYPE = (
+        ("up", "Up Vote"),
+        ("down", "Down Vote"),
+    )
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    body = models.TextField(null=True, blank=True)
+    vote_type = models.CharField(max_length=50, choices=VOTE_TYPE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """
+        This method returns the vote type of the review
+        """
+        return self.vote_type
+
+
+class Tag(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
---
+++ projects/views.py
@@ -0,0 +1,74 @@
+from django.http import HttpRequest
+from django.shortcuts import redirect, render
+
+from .models import Project
+from .forms import ProjectForm
+
+
+def projects(request: HttpRequest):
+    """
+    This function renders all the projects.
+    """
+    return render(
+        request,
+        "projects/projects.html",
+        {"projects": Project.objects.all()},
+    )
+
+
+def project(request: HttpRequest, project_id: str):
+    """
+    This function is used to render the project page.
+    """
+    return render(
+        request,
+        "projects/project.html",
+        {"project": Project.objects.get(id=project_id)},
+    )
+
+
+def create_project(request: HttpRequest):
+    """
+    This function is used to render the create project page.
+    """
+    form = ProjectForm()
+
+    if request.method == "POST":
+        form = ProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("projects")
+
+    context = {"form": form}
+    return render(request, "projects/project_form.html", context=context)
+
+
+def update_project(request: HttpRequest, project_id: str):
+    """
+    This function is used to render the update project page.
+    """
+    project = Project.objects.get(id=project_id)
+    form = ProjectForm(instance=project)
+
+    if request.method == "POST":
+        form = ProjectForm(request.POST, request.FILES ,instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect("projects")
+
+    context = {"form": form}
+    return render(request, "projects/project_form.html", context=context)
+
+
+def delete_project(request: HttpRequest, project_id: str):
+    """
+    This function is used to delete a project.
+    """
+    object = Project.objects.get(id=project_id)
+    context = {"object": object}
+
+    if request.method == "POST":
+        object.delete()
+        return redirect("projects")
+
+    return render(request, "projects/delete_template.html", context)
